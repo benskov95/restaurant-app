0b69782edda503e4c814b507aa5dbc0769428e63
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-08-01 21:03:04 +0200

    done with first client iteration

--- restaurant-client/src/components/ContactPage.vue
+++ restaurant-client/src/components/ContactPage.vue
@@ -1,10 +1,35 @@
 <template>
-    <div>
-        <h1>Contact us</h1>
-        <fa-icon icon="fa-solid fa-bars" />
+    <div id="contact-page">
+        <h1>Restaurant</h1>
+        <h3>You can find us at:</h3>
+        <ul>
+            <li>
+                <fa-icon icon="fa-solid fa-phone" /> +45 12 34 56 78
+            </li>
+            <li>
+                <fa-icon icon="fa-solid fa-envelope" /> restaurant@mail.com
+            </li>
+            <li>
+                <fa-icon icon="fa-solid fa-location" /> Random St. 123, Somewhere
+            </li>
+        </ul>
     </div>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+    font-size: 2.5rem;
+}
+ul {
+    list-style: none;
+    display: inline-block;
+    text-align: left;
+}
+li {
+    font-size: 1.5rem;
+    padding: 0.5rem 0px 0.5rem 0px;
+    border-bottom: 1px solid gray;
+}
+</style>
